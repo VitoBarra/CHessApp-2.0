afe50e8387589efc59d66e5fcda7af1e433c0963
Vito Barra <tovi.2309@gmail.com>
2018-09-10 18:25:38 +0200

    Add Option Dimension

--- ChessApp 2.0/Form1.cs
+++ ChessApp 2.0/Form1.cs
@@ -13,6 +13,7 @@ using System.IO;
 using Svg;
 using Microsoft.Scripting.Hosting;
 
+
 using IronPython.Modules;
 using IronPython.Runtime;
 using IronPython.Hosting;
@@ -25,7 +26,6 @@ namespace ChessApp_2._0
 {
     public partial class Form1 : Form
     {
-       
         Thread PyThread = new Thread(PythonIni);
 
         bool clickdStart = false;
@@ -41,23 +41,34 @@ namespace ChessApp_2._0
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        public void Form1_Load(object sender, EventArgs e)
         {
+            
+
             Global.FileReader = new StreamReader(pathStr + "\\GameMode.txt");
             gameMode = Global.FileReader.ReadLine();
             Global.FileReader.Close();
 
-            Bildboard();
-            tLeft = new System.Timers.Timer();
-            tLeft.Interval = 1000;
+            Global.SvgBitMap = LoadSvg();
+
+
+            tLeft = new System.Timers.Timer() { Interval = 1000};
+            
             tLeft.Elapsed += OnTimeEventLeft;
             LeftTimerButton.Text = leftMinutes + ":" + leftSecond;
 
-            tRight = new System.Timers.Timer();
-            tRight.Interval = 1000;
+            tRight = new System.Timers.Timer() { Interval = 1000 };
+           
             tRight.Elapsed += OnTimeEventRight;
             RightTimerButton.Text = rightMinutes + ":" + rightSecond;
+
+
+            
+
+            Bildboard();
         }
+
+
 
         private void Start_Click(object sender, EventArgs e)
         {
@@ -117,153 +128,6 @@ namespace ChessApp_2._0
           
         }
 
-        //------------------------------------------------------method Form1-----------------------------------
-        #region ------------------------board-------------------
-        private void Bildboard()
-        {
-            int offset = 0;
-            int pixelPice = 50;
-            Global.board = new Board[8, 8];
-            Global.boardCod = new int[8, 8]
-               {{4,3,2,6,5,2,3,4 },
-                 {1,1,1,1,1,1,1,1 },
-                 {0,0,0,0,0,0,0,0 },
-                 {0,0,0,0,0,0,0,0 },
-                 {0,0,0,0,0,0,0,0 },
-                 {0,0,0,0,0,0,0,0 },
-                 {-1,-1,-1,-1,-1,-1,-1,-1 },
-                 {-4,-3,-2,-6,-5,-2,-3,-4 } };
-            //----------------------------------board AREA----------------------------------
-
-            for (int i = 0; i < 8; i++)
-                for (int j = 0; j < 8; j++)
-                {
-                    Global.board[i, j] = new Board();
-                    Global.board[i, j].xPos = j;
-                    Global.board[i, j].yPos = i;
-                    Global.board[i, j].Parent = this;
-                    Global.board[i, j].Location = new Point(j * pixelPice + offset, i * pixelPice + pixelPice);
-                    Global.board[i, j].Size = new Size(pixelPice, pixelPice);
-
-                    if (i % 2 == 0)
-                    {
-                        if (j % 2 == 1)
-                            Global.board[i, j].BackColor = ColorTranslator.FromHtml("#744b44");
-                        else
-                            Global.board[i, j].BackColor = ColorTranslator.FromHtml("#f4e4b5");
-                    }
-                    else
-                    {
-                        if (j % 2 == 0)
-                            Global.board[i, j].BackColor = ColorTranslator.FromHtml("#744b44");
-                        else
-                            Global.board[i, j].BackColor = ColorTranslator.FromHtml("#f4e4b5");
-                    }
-                }
-
-            RenderPiceOnboard();
-        }
-        public static void RenderPiceOnboard()
-        {
-            SvgDocument document = SvgDocument.Open("pice\\PedestrianW.svg");
-            document.Width = 50;
-            document.Height = 50;
-            Bitmap bp = document.Draw();
-
-            for (int i = 0; i < 8; i++)
-                for (int j = 0; j < 8; j++)
-                {
-                    switch (Global.boardCod[i, j])
-                    {
-                        case 0: Global.board[i, j].BackgroundImage = null; break;
-                        case 1: Global.board[i, j].BackgroundImage = bp ; break;
-                        case -1: Global.board[i, j].BackgroundImage = System.Drawing.Image.FromFile("pice\\PedestrianB.png"); break;
-                        case 4: Global.board[i, j].BackgroundImage = System.Drawing.Image.FromFile("pice\\TowerW.png"); break;
-                        case -4: Global.board[i, j].BackgroundImage = System.Drawing.Image.FromFile("pice\\TowerB.png"); break;
-                        case 3: Global.board[i, j].BackgroundImage = System.Drawing.Image.FromFile("pice\\HorseW.png"); break;
-                        case -3: Global.board[i, j].BackgroundImage = System.Drawing.Image.FromFile("pice\\HorseB.png"); break;
-                        case 2: Global.board[i, j].BackgroundImage = System.Drawing.Image.FromFile("pice\\BishopW.png"); break;
-                        case -2: Global.board[i, j].BackgroundImage = System.Drawing.Image.FromFile("pice\\BishopB.png"); break;
-                        case 5: Global.board[i, j].BackgroundImage = System.Drawing.Image.FromFile("pice\\QueenW.png"); break;
-                        case -5: Global.board[i, j].BackgroundImage = System.Drawing.Image.FromFile("pice\\QueenB.png"); break;
-                        case 6: Global.board[i, j].BackgroundImage = System.Drawing.Image.FromFile("pice\\KingW.png"); break;
-                        case -6: Global.board[i, j].BackgroundImage = System.Drawing.Image.FromFile("pice\\KingB.png"); break;
-                    }
-
-                }
-        }
-        private static void PythonIni()
-        {
-
-            ScriptEngine engine = Python.CreateEngine();
-
-
-            var paths = engine.GetSearchPaths();
-            paths.Add(@"C:\Python27\Lib");
-            paths.Add(@"c:\python27\lib\site-packages\numpy\core");
-            paths.Add(pathStr);
-            engine.SetSearchPaths(paths);
-
-            CompiledCode compiledCode = engine.CreateScriptSourceFromFile(pathStr + gameMode).Compile();
-
-           
-
-            ScriptScope scope = engine.CreateScope();
-
-            PythonPass pass = new PythonPass();
-            scope.SetVariable("PythonPass", pass);
-
-
-            compiledCode.Execute(scope);
-
-        }
-        private void RefreshButton_Click(object sender, EventArgs e)
-        {
-            SvgDocument document = SvgDocument.Open("pice\\PedestrianW.svg");
-
-            document.Width = int.Parse(Width.Text);
-            document.Height = int.Parse(Width.Text);
-
-            pictureBox1.Width = int.Parse(Width.Text);
-            pictureBox1.Height = int.Parse(Width.Text);
-
-            Bitmap bp = document.Draw();
-
-
-
-            pictureBox1.Image = bp;
-        }
-
-        public static string FindPath()
-        {
-            return Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).ToString()
-                ).ToString()).ToString()+ "\\ScriptChess";
-        }
-        #endregion
-
-        
-
-
-
-
-
-        #region ----------------------track----------------------
-        private void TrackBackButton_Click(object sender, EventArgs e)
-        {
-            PythonPass.Trackback = true;
-        }
-
-        private void TrackForwardButton_Click(object sender, EventArgs e)
-        {
-
-        }
-        #endregion
-
-
-
-
-
-
         #region -------------ToolStrip item function-------------
 
 
@@ -321,15 +185,179 @@ namespace ChessApp_2._0
             Global.FileWriter.Close();
         }
 
-
-
         private void SetPositionTool_Click(object sender, EventArgs e)
         {
 
         }
 
 
+
+        private void OptionTool_Click(object sender, EventArgs e)
+        {
+            OptionForm optionForm = new OptionForm { StartPosition = FormStartPosition.CenterParent };
+            optionForm.ShowDialog();
+
+
+            Global.FileWriter = new StreamWriter("Dimention.txt", false);
+
+
+
+            Global.SvgBitMap = LoadSvg();
+            Bildboard();
+
+        }
+
+
         #endregion
+
+
+        //------------------------------------------------------method Form1-----------------------------------
+        #region ------------------------board-------------------
+        public void Bildboard()
+        {
+            this.Width = Global.width_Height * 8 + 15;
+            this.Height = Global.width_Height * 8 + Global.width_Height + 39;
+
+            int offset = 0;
+            int pixelPice = Global.width_Height;
+            if (Global.board == null)
+                Global.board = new Board[8, 8];
+            Global.boardCod = new int[8, 8]
+               {{4,3,2,6,5,2,3,4 },
+                 {1,1,1,1,1,1,1,1 },
+                 {0,0,0,0,0,0,0,0 },
+                 {0,0,0,0,0,0,0,0 },
+                 {0,0,0,0,0,0,0,0 },
+                 {0,0,0,0,0,0,0,0 },
+                 {-1,-1,-1,-1,-1,-1,-1,-1 },
+                 {-4,-3,-2,-6,-5,-2,-3,-4 } };
+            //----------------------------------board AREA----------------------------------
+
+            for (int i = 0; i < 8; i++)
+                for (int j = 0; j < 8; j++)
+                {
+                    if (Global.board[i, j] == null)
+                        Global.board[i, j] = new Board();
+                    Global.board[i, j].xPos = j;
+                    Global.board[i, j].yPos = i;
+                    Global.board[i, j].Parent = this;
+                    Global.board[i, j].Location = new Point(j * pixelPice + offset, i * pixelPice + pixelPice);
+                    Global.board[i, j].Size = new Size(pixelPice, pixelPice);
+
+                    if (i % 2 == 0)
+                    {
+                        if (j % 2 == 1)
+                            Global.board[i, j].BackColor = ColorTranslator.FromHtml("#744b44");
+                        else
+                            Global.board[i, j].BackColor = ColorTranslator.FromHtml("#f4e4b5");
+                    }
+                    else
+                    {
+                        if (j % 2 == 0)
+                            Global.board[i, j].BackColor = ColorTranslator.FromHtml("#744b44");
+                        else
+                            Global.board[i, j].BackColor = ColorTranslator.FromHtml("#f4e4b5");
+                    }
+                }
+
+            RenderPiceOnboard();
+        }
+
+        private Bitmap[] LoadSvg()
+        {
+            int count = 0;
+            SvgDocument[] document = new SvgDocument[12];
+            Bitmap[] bp = new Bitmap[12];
+            string[] Files = Directory.GetFiles("pice");
+            foreach(string File in Files)
+            {
+                document[count] = SvgDocument.Open(File);
+                document[count].Width = Global.width_Height;
+                document[count].Height = Global.width_Height;
+                bp[count] = document[count].Draw();
+                count++;
+            }
+            return bp;
+        }
+
+        public static void RenderPiceOnboard()
+        {
+            for (int i = 0; i < 8; i++)
+                for (int j = 0; j < 8; j++)
+                {
+                    switch (Global.boardCod[i, j])
+                    {
+                        case 0: Global.board[i, j].BackgroundImage = null; break;
+                        case 1: Global.board[i, j].BackgroundImage = Global.SvgBitMap[7]; break;
+                        case -1: Global.board[i, j].BackgroundImage = Global.SvgBitMap[6]; break;
+                        case 4: Global.board[i, j].BackgroundImage = Global.SvgBitMap[11]; break;
+                        case -4: Global.board[i, j].BackgroundImage = Global.SvgBitMap[10]; break;
+                        case 3: Global.board[i, j].BackgroundImage = Global.SvgBitMap[3]; break;
+                        case -3: Global.board[i, j].BackgroundImage = Global.SvgBitMap[2]; break;
+                        case 2: Global.board[i, j].BackgroundImage = Global.SvgBitMap[1]; break;
+                        case -2: Global.board[i, j].BackgroundImage = Global.SvgBitMap[0]; break;
+                        case 5: Global.board[i, j].BackgroundImage = Global.SvgBitMap[9]; break;
+                        case -5: Global.board[i, j].BackgroundImage = Global.SvgBitMap[8]; break;
+                        case 6: Global.board[i, j].BackgroundImage = Global.SvgBitMap[5]; break;
+                        case -6: Global.board[i, j].BackgroundImage = Global.SvgBitMap[4]; break;
+                    }
+
+                }
+        }
+        private static void PythonIni()
+        {
+            ScriptEngine engine = Python.CreateEngine();
+
+
+            var paths = engine.GetSearchPaths();
+            paths.Add(@"C:\Python27\Lib");
+            paths.Add(@"c:\python27\lib\site-packages\numpy\core");
+            paths.Add(pathStr);
+            engine.SetSearchPaths(paths);
+
+            CompiledCode compiledCode = engine.CreateScriptSourceFromFile(pathStr + gameMode).Compile();
+
+           
+
+            ScriptScope scope = engine.CreateScope();
+
+            PythonPass pass = new PythonPass();
+            scope.SetVariable("PythonPass", pass);
+
+
+            compiledCode.Execute(scope);
+
+        }
+        public static string FindPath()
+        {
+            return Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).ToString()
+                ).ToString()).ToString()+ "\\ScriptChess";
+        }
+        #endregion
+
+        
+
+
+
+
+
+        #region ----------------------track----------------------
+        private void TrackBackButton_Click(object sender, EventArgs e)
+        {
+            PythonPass.Trackback = true;
+        }
+
+        private void TrackForwardButton_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+
+
+
+
+
 
 
 
@@ -423,8 +451,11 @@ namespace ChessApp_2._0
 
     public static class Global
     {
+
         public static StreamReader FileReader;
         public static StreamWriter FileWriter;
+        public static Bitmap[] SvgBitMap;
+        public static int width_Height = 50;
         public static Board[,] board;
         public static int[,] boardCod;
         public static bool Player = false;
---
+++ ChessApp 2.0/OptionForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChessApp_2._0
+{
+    public partial class OptionForm : Form
+    {
+        public OptionForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OKButton_Click(object sender, EventArgs e)
+        {
+            Global.width_Height = int.Parse(DimentionSquare.Text);
+            this.Close();
+        }
+
+        private void ApplicaButton_Click(object sender, EventArgs e)
+        {
+            Global.width_Height = int.Parse(DimentionSquare.Text);
+        }
+
+        private void CancellaButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
